a15ed11167f06f53dbd4946ce8943f7b5f89115e
zrumyk <148488442+zrumyk@users.noreply.github.com>
2026-04-17 07:47:02 +0300

    fixed api url

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const apiClient = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
